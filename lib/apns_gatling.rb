541fc89aab893f975053d16d7ea03cdfacd20b11
Cloud <cloudcry@gmail.com>
2017-04-23 00:02:06 +0800

    imports

--- lib/apns_gatling.rb
+++ lib/apns_gatling.rb
@@ -1,4 +1,9 @@
 require "apns_gatling/version"
+require "apns_gatling/apns_client"
+require "apns_gatling/jwt_token"
+require "apns_gatling/message"
+require "apns_gatling/request"
+require "apns_gatling/response"
 
 module ApnsGatling
   # Your code goes here...
