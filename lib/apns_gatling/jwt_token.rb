391db30821465a8ab2d84cc90e0c4904ec30cd8d
Cloud <cloudcry@gmail.com>
2017-04-22 23:59:29 +0800

    jwt token

--- lib/apns_gatling/jwt_token.rb
+++ lib/apns_gatling/jwt_token.rb
@@ -1,6 +1,19 @@
 require 'jwt'
 
 module ApnsGatling
-    class Token
+  class Token
+    attr_reader :team_id, :auth_key_id, :ecdsa_key
+
+    def initialize(team_id, auth_key_id, ecdsa_key)
+      @team_id = team_id
+      @auth_key_id = auth_key_id
+      @ecdsa_key = ecdsa_key
     end
+
+    def new_token
+      payload = {iss: @team_id, iat: Time.now.to_i}
+      header = {kid: @auth_key_id}
+      JWT.encode payload, @ecdsa_key, 'ES256', header
+    end
+  end
 end
