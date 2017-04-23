db94527677975a090d35796841f51ad01d58d824
Cloud <cloudcry@gmail.com>
2017-04-23 12:34:56 +0800

    check and update message definition

--- lib/apns_gatling/message.rb
+++ lib/apns_gatling/message.rb
@@ -3,21 +3,27 @@ require 'securerandom'
 
 module ApnsGatling
   class Message
+    # see: https://developer.apple.com/library/content/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/PayloadKeyReference.html#//apple_ref/doc/uid/TP40008194-CH17-SW1
+    MAXIMUM_PAYLOAD_SIZE = 4096
+
     attr_reader :token
-    attr_accessor :alert, :badge, :sound, :content_available, :category, :custom_payload, :url_args, :mutable_content
+    attr_accessor :alert, :badge, :sound, :content_available, :category, :custom_payload, :thread_id
     attr_accessor :apns_id, :expiration, :priority, :topic, :apns_collapse_id
 
-  def initialize(token)
+   def initialize(token)
      @token = token
      @apns_id = SecureRandom.uuid
    end
 
-   def data
-     JSON.dump(to_hash).force_encoding(Encoding::BINARY)
+   def payload_data
+     payload.to_json.force_encoding(Encoding::BINARY)
    end
 
-   private
-   def to_hash
+   def valid?
+     data.bytesize <= MAXIMUM_PAYLOAD_SIZE
+   end
+
+   def payload
      aps = {}
 
      aps.merge!(alert: alert) if alert
@@ -25,10 +31,9 @@ module ApnsGatling
      aps.merge!(sound: sound) if sound
      aps.merge!(category: category) if category
      aps.merge!('content-available' => content_available) if content_available
-     aps.merge!('url-args' => url_args) if url_args
-     aps.merge!('mutable-content' => mutable_content) if mutable_content
+     aps.merge!('thread-id' => thread_id) if thread_id
 
-     message = { aps: aps }
+     message = {aps: aps}
      message.merge!(custom_payload) if custom_payload
      message
    end
