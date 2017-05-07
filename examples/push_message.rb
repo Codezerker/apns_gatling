cda5684fb24370833f71514b6436dcfc15f54d16
Cloud <cloudcry@gmail.com>
2017-05-07 18:11:20 +0800

    update example

--- examples/push_message.rb
+++ examples/push_message.rb
@@ -1,32 +1,25 @@
 require 'apns_gatling'
 require 'openssl'
 
-team_id = '<your team id>'
-auth_key_id = '<your auth key id>'
-auth_key_file = '<your p8 file>'
+team_id = '<Your team id>'
+auth_key_id = '<Your Dev auth p8 file key>'
+auth_key_file = '<Your Dev auth p8 file>'
 ecdsa_key = OpenSSL::PKey::EC.new File.read auth_key_file
 
 def message(body)
   msg = ApnsGatling::Message.new '<device token>'
   msg.alert = {title: "test", body: body}
-  msg.topic = '<app bundle id>'
+  msg.topic = '<Your App Bundle ID>'
   msg
 end
 
 client = ApnsGatling::Client.new team_id, auth_key_id, ecdsa_key, true
 
-puts "messages on the way..."
-client.push(message("test 1")) do |response|
-  puts response.ok?, response.error
-  if response.ok? 
-    6.times do |i|
-      puts "num #{i}"
-      client.push(message("test #{i}")) do |r|
-         puts r.ok?, r.error
-      end
-    end
+6.times do |i|
+  puts "num #{i}"
+  client.push(message("test #{i}")) do |r|
+    puts "num #{i} success: #{r.ok?}, error: #{r.error}"
   end
 end
-puts ">>>> Waiting... <<<<"
-client.join
 
+client.join
